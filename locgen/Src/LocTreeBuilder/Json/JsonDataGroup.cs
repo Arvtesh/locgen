ae199f78f871b5aa1391e2a5c8e0935280d08cd6
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2017-06-23 20:17:47 +0300

    added json data structure

---
+++ locgen/Src/LocTreeBuilder/Json/JsonDataGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace locgen.Impl
+{
+	internal class JsonDataGroup : JsonDataItem
+	{
+		public JsonDataGroup[] Groups { get; set; }
+		public JsonDataUnit[] Units { get; set; }
+	}
+}
